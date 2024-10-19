8085a941d1e64accc69270e5a86641ed70d3443a
arshia-hr <arshiahvgrdi@gmail.com>
2024-10-19 14:17:40 -0700

    add required tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
-    backgroundColor:{
-      'background--primery-1': '#47d0e8',
-      'background--primery-2': '#eaebf6',
-      'background--secondery-1': '#090cd0',
-      'background--secondery-2': '#483ea6'
+    extend: {
+      backgroundColor:{
+        'background--primery-1': '#47d0e8',
+        'background--primery-2': '#eaebf6',
+        'background--secondery-1': '#090cd0',
+        'background--secondery-2': '#483ea6'
+      },
+      colors:{
+        'color--primery': '#000',
+        'color--secondery': '#fff'
+      }
     },
-    colors:{
-      'color--primery': '#000',
-      'color--secondery': '#fff'
-    }
   },
   plugins: [],
 }
