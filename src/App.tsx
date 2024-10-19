d2f0a24a6620322c98e0f643b3be9dd1ee78d9cc
arshia-hr <arshiahvgrdi@gmail.com>
2024-10-19 13:40:26 -0700

    add state darkmode and handleClick for change value dark mode and put to condition for main background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 
 function App() {
-
+  const [darkMode,setDarkMode] = useState<boolean>(false)
+  const handleClick = () => {
+    setDarkMode(darkMode => darkMode ? false : true)
+  }
   return (
-    <>
-
-    </>
+    <div className={darkMode ? `bg-background--secondery-1` : `bg-background--primery-1`}>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
