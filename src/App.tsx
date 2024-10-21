1eb71eeda3eaae57c9d45a3223e05290cd0f2993
arshia-hr <arshiahvgrdi@gmail.com>
2024-10-21 13:28:39 -0700

    add selector for cities and add some  condition for darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import rain from "././assets/Rain.svg";
-import { styled, Switch, SwitchProps } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select, styled, Switch} from "@mui/material";
 
 const cities = [
   {
@@ -25,74 +25,64 @@ const cities = [
   },
 ];
 function App() {
-  const IOSSwitch = styled((props: SwitchProps) => (
-    <Switch
-      focusVisibleClassName=".Mui-focusVisible"
-      disableRipple
-      {...props}
-    />
-  ))(({ theme }) => ({
-    width: 42,
-    height: 26,
-    padding: 0,
-    "& .MuiSwitch-switchBase": {
+  const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    width: 62,
+    height: 34,
+    padding: 7,
+    '& .MuiSwitch-switchBase': {
+      margin: 1,
       padding: 0,
-      margin: 2,
-      transitionDuration: "300ms",
-      "&.Mui-checked": {
-        transform: "translateX(16px)",
-        color: "#fff",
-        "& + .MuiSwitch-track": {
-          backgroundColor: "#65C466",
+      transform: 'translateX(6px)',
+      '&.Mui-checked': {
+        color: '#fff',
+        transform: 'translateX(22px)',
+        '& .MuiSwitch-thumb:before': {
+          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+            '#fff',
+          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+        },
+        '& + .MuiSwitch-track': {
           opacity: 1,
-          border: 0,
-          ...theme.applyStyles("dark", {
-            backgroundColor: "#2ECA45",
+          backgroundColor: '#aab4be',
+          ...theme.applyStyles('dark', {
+            backgroundColor: '#8796A5',
           }),
         },
-        "&.Mui-disabled + .MuiSwitch-track": {
-          opacity: 0.5,
-        },
-      },
-      "&.Mui-focusVisible .MuiSwitch-thumb": {
-        color: "#33cf4d",
-        border: "6px solid #fff",
-      },
-      "&.Mui-disabled .MuiSwitch-thumb": {
-        color: theme.palette.grey[100],
-        ...theme.applyStyles("dark", {
-          color: theme.palette.grey[600],
-        }),
-      },
-      "&.Mui-disabled + .MuiSwitch-track": {
-        opacity: 0.7,
-        ...theme.applyStyles("dark", {
-          opacity: 0.3,
-        }),
       },
     },
-    "& .MuiSwitch-thumb": {
-      boxSizing: "border-box",
-      width: 22,
-      height: 22,
-    },
-    "& .MuiSwitch-track": {
-      borderRadius: 26 / 2,
-      backgroundColor: "#E9E9EA",
-      opacity: 1,
-      transition: theme.transitions.create(["background-color"], {
-        duration: 500,
+    '& .MuiSwitch-thumb': {
+      backgroundColor: '#001e3c',
+      width: 32,
+      height: 32,
+      '&::before': {
+        content: "''",
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        left: 0,
+        top: 0,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+      },
+      ...theme.applyStyles('dark', {
+        backgroundColor: '#003892',
       }),
-      ...theme.applyStyles("dark", {
-        backgroundColor: "#39393D",
+    },
+    '& .MuiSwitch-track': {
+      opacity: 1,
+      backgroundColor: '#aab4be',
+      borderRadius: 20 / 2,
+      ...theme.applyStyles('dark', {
+        backgroundColor: '#8796A5',
       }),
     },
   }));
-
   const [darkMode, setDarkMode] = useState<boolean>(false);
-  console.log(darkMode);
-  const handleClick = () => {
-    setDarkMode((darkMode) => (darkMode ? false : true));
+  const handleClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDarkMode(e.target.checked)
   };
   return (
     <div
@@ -101,28 +91,41 @@ function App() {
       }`}
     >
       <div
-        className={` w-[60%]  relative rounded-lg h-[50%] flex flex-col  ${
+        className={` w-[60%]  relative  rounded-lg h-[50%] flex flex-col  ${
           darkMode ? "bg-background--secondery-2" : "bg-background--primery-2"
         }`}
       >
-        <IOSSwitch
-          onClick={() => handleClick}
-          className="absolute left-0 top-0"
+        <MaterialUISwitch   checked={darkMode}
+          onChange={(e) => handleClick(e)}
+          className="absolute left-10 top-5"
         />
         <div>
           <img src={rain} alt="img-weather" className="w-[40%] m-auto" />
         </div>
-        <div className="border-t-2 w-[70%] relative mx-auto border-[#000]">
-          <ul className="flex pt-3 before:absolute before:top-0 before:left-32 before:h-full before:w-[2px] after:absolute after:top-0 after:right-32 after:h-full after:w-[2px] after:bg-black before:bg-black justify-around text-center">
-            <li className="flex flex-col">
+        <FormControl sx={{width: '50%',margin: '0px auto'}}>
+          <InputLabel id="demo-simple-select-label">Cities</InputLabel>
+          <Select 
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="City"
+          >
+            <MenuItem >Ten</MenuItem>
+            <MenuItem value={100}>Twenty</MenuItem>
+            <MenuItem >Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        <br/>
+        <div className={`border-t-2 w-[70%] relative mx-auto ${darkMode ? 'border-color--secondery' : 'border-color--primery' }`}>
+          <ul className={`flex pt-3 before:absolute before:top-0 before:left-32 before:h-full before:w-[2px] after:absolute after:top-0 after:right-32 after:h-full after:w-[2px] ${darkMode ? 'after:bg-color--secondery' : 'after:bg-color--primery' } ${darkMode ? 'before:bg-color--secondery' : 'before:bg-color--primery' } justify-around text-center`}>
+            <li className={` flex  flex-col ${darkMode ? 'text-color--secondery' : 'text-color--primery' } `}>
               <a href="#">aa</a>
               <a href="#">a</a>
             </li>
-            <li className=" flex  flex-col ">
+            <li className={` flex  flex-col ${darkMode ? 'text-color--secondery' : 'text-color--primery' } `}>
               <a href="#">a</a>
               <a href="#">a</a>
             </li>
-            <li className="flex text-center flex-col">
+            <li className={` flex  flex-col ${darkMode ? 'text-color--secondery' : 'text-color--primery' } `}>
               <a href="#">a</a>
               <a href="#">a</a>
             </li>
