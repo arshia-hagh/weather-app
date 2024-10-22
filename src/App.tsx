34eab66f658e6ce8e449a1f7092e380552bf108b
arshia-hr <arshiahvgrdi@gmail.com>
2024-10-22 14:15:24 -0700

    add condition for img show status weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import rain from "././assets/Rain.svg";
+import snow from '././assets/Snow.svg'
+import drizzle from '././assets/Drizzle.svg'
+import clouds from '././assets/Clouds.svg'
+import storm from '././assets/Thunderstorm.svg'
+import clearD from '././assets/Clear-Day.svg'
+import clearN from '././assets/Clear-Night.svg'
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, styled, Switch} from "@mui/material";
 import { getWeather } from "./services/api";
-import { TWeather } from "./server/type";
+import { TWeather, Weather } from "./server/type";
 
 interface TCity{
   id: string
@@ -36,62 +42,63 @@ const cities = [
     lon: "59.6067",
   },
 ];
-function App() {
-  const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-    width: 62,
-    height: 34,
-    padding: 7,
-    '& .MuiSwitch-switchBase': {
-      margin: 1,
-      padding: 0,
-      transform: 'translateX(6px)',
-      '&.Mui-checked': {
-        color: '#fff',
-        transform: 'translateX(22px)',
-        '& .MuiSwitch-thumb:before': {
-          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-            '#fff',
-          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-        },
-        '& + .MuiSwitch-track': {
-          opacity: 1,
-          backgroundColor: '#aab4be',
-          ...theme.applyStyles('dark', {
-            backgroundColor: '#8796A5',
-          }),
-        },
-      },
-    },
-    '& .MuiSwitch-thumb': {
-      backgroundColor: '#001e3c',
-      width: 32,
-      height: 32,
-      '&::before': {
-        content: "''",
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        left: 0,
-        top: 0,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  '& .MuiSwitch-switchBase': {
+    margin: 1,
+    padding: 0,
+    transform: 'translateX(6px)',
+    '&.Mui-checked': {
+      color: '#fff',
+      transform: 'translateX(22px)',
+      '& .MuiSwitch-thumb:before': {
         backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
           '#fff',
-        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
       },
-      ...theme.applyStyles('dark', {
-        backgroundColor: '#003892',
-      }),
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: '#aab4be',
+        ...theme.applyStyles('dark', {
+          backgroundColor: '#8796A5',
+        }),
+      },
     },
-    '& .MuiSwitch-track': {
-      opacity: 1,
-      backgroundColor: '#aab4be',
-      borderRadius: 20 / 2,
-      ...theme.applyStyles('dark', {
-        backgroundColor: '#8796A5',
-      }),
+  },
+  '& .MuiSwitch-thumb': {
+    backgroundColor: '#001e3c',
+    width: 32,
+    height: 32,
+    '&::before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        '#fff',
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
     },
-  }));
+    ...theme.applyStyles('dark', {
+      backgroundColor: '#003892',
+    }),
+  },
+  '& .MuiSwitch-track': {
+    opacity: 1,
+    backgroundColor: '#aab4be',
+    borderRadius: 20 / 2,
+    ...theme.applyStyles('dark', {
+      backgroundColor: '#8796A5',
+    }),
+  },
+}));
+function App() {
+ 
   const [darkMode, setDarkMode] = useState<boolean>(false);
   const [dataWe,setDataWe] = useState<TWeather>()
   const handleClick = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -104,7 +111,8 @@ function App() {
       setDataWe(result)
     })
   }
-
+  const main = dataWe?.weather.find(items => items.id === items.id)?.main
+  console.log(main)
   return (
     <div
       className={` h-screen   flex items-center justify-center ${
@@ -121,7 +129,7 @@ function App() {
           className="absolute left-10 top-5"
         />
         <div>
-          <img src={rain} alt="img-weather" className="w-[30%] mx-auto" />
+          <img src={iconsimg(main as string)}alt="img-weather" className="w-[30%] mx-auto" />
         </div>
         <FormControl sx={{width: '50%',margin: '0px auto'}}>
           <InputLabel id="demo-simple-select-label">Cities</InputLabel>
@@ -145,7 +153,7 @@ function App() {
             </li>
             <li className={` flex font-bold  flex-col ${darkMode ? 'text-color--secondery' : 'text-color--primery' } `}>
               <a href="#">DISCRIPTION</a>
-              <a href="#">{dataWe?.weather.map(item => item.main)}</a>
+              <a href="#">{dataWe?.weather.map(item => item.description)}</a>
             </li>
             <li className={` flex font-bold  flex-col ${darkMode ? 'text-color--secondery' : 'text-color--primery' } `}>
               <a href="#">HUMIDITY</a>
@@ -157,5 +165,37 @@ function App() {
     </div>
   );
 }
+function dayornight(){
+  const h = new Date().getHours()
+  if(h >= 7 && h <= 14){
+    return clearD
+  }
 
+  else{
+    return clearN
+  }
+}
+function iconsimg(icon : string){
+  switch (icon) {
+    case 'Rain':
+      return rain
+      break;
+    case 'Clear':
+      return dayornight()
+      break;
+    case 'Clouds':
+      return clouds
+      break;
+    case 'Drizzle':
+      return drizzle
+      break;
+    case 'Thunderstrom':
+      return storm
+      break;
+    case 'Snow':
+      return snow
+      break;
+    
+  }
+}
 export default App;
